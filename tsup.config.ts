626573a9472cce6ad64fc88c6e073bf7db37e5ce
Enrico <camponogara5@gmail.com>
2024-09-27 11:46:24 -0300

    fix(build): corrige build

---
+++ tsup.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	// The file we created above that will be the entrypoint to the library.
+	entry: [
+		"lib/**/*.ts",
+		"lib/**/*.tsx",
+		"!lib/fixtures",
+		"!lib/**/*.test.ts",
+		"!lib/**/*.test.tsx",
+	],
+	minify: true,
+	target: "es2018",
+	external: ["react", "react-dom"],
+	sourcemap: false,
+	dts: true,
+	format: ["cjs", "esm"],
+	clean: true,
+	bundle: false,
+	tsconfig: "./tsconfig.json",
+});
