2e0534029ede2a6b7392845c3c9cd85bff88d0b5
Enrico <camponogara5@gmail.com>
2024-09-27 13:30:18 -0300

    fix(packages): corrige import

--- tsup.config.ts
+++ tsup.config.ts
@@ -15,6 +15,11 @@ export default defineConfig({
 	sourcemap: false,
 	dts: true,
 	format: ["cjs", "esm"],
+	outExtension(ctx) {
+		return {
+			js: ctx.format === "esm" ? ".mjs" : ".js",
+		};
+	},
 	clean: true,
 	bundle: false,
 	tsconfig: "./tsconfig.json",
